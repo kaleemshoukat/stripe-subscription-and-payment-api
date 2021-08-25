a602ffb353ddce7049e094efe87802fec21801ca
kaleem <ks@softwarealliance.dk>
2021-08-25 13:18:19 +0500

    webhook added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\SubscriptionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/webhook-subscription", [SubscriptionController::class, 'webhookSubscription'])->name('webhookSubscription');
